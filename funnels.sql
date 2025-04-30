8223ced0f33b533b9a29a18867f1e1278a776850
Kristina Rakovskaja <kristina.rakovskaja@gmail.com>
2025-04-30 19:55:30 +0300

    Update funnels.sql

--- funnels.sql
+++ funnels.sql
@@ -1,42 +1,4 @@
---Query for unique events					
-					
-WITH ranked_events AS (					
-SELECT					
-ROW_NUMBER() OVER (PARTITION BY user_pseudo_id, event_name ORDER BY event_timestamp) AS row_num,					
-*					
-FROM `tc-da-1.turing_data_analytics.raw_events`)					
-					
-					
-SELECT					
-*					
-FROM ranked_events					
-WHERE row_num = 1
-
-
--- Query that aggregates total number of events per top 3 countries		
-		
-WITH ranked_events AS (		
-SELECT		
-ROW_NUMBER() OVER (PARTITION BY user_pseudo_id, event_name ORDER BY event_timestamp) AS row_num,		
-*		
-FROM `tc-da-1.turing_data_analytics.raw_events`),		
-		
-unique_events AS (SELECT		
-* 		
-FROM ranked_events		
-WHERE row_num = 1)		
-		
-SELECT		
-country,		
-COUNT(*) AS num_events		
-FROM unique_events		
-WHERE event_name IN ("view_item", "add_to_cart", "begin_checkout", "add_shipping_info", "add_payment_info", "purchase")		
-GROUP BY country		
-ORDER BY num_events DESC		
-LIMIT 3			
-
-
--- Query that aggregates the number of identified events  per top 3 countries
+-- Query that aggregates the number of identified events  per the top 3 countries
 	
 WITH ranked_events AS (	
 SELECT	
